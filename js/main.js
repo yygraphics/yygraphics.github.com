c48347e5ef0d3aab92566bdac54ea5b3811905d1
yygraphics <picple.j@gmail.com>
2017-12-28 07:59:01 +0700

    some issues fixed
    태그 플러그인 추가 전에 커밋

--- js/main.js
+++ js/main.js
@@ -2,14 +2,39 @@
 new PerfectScrollbar("#gnb");
 
 
-/* 시간에 따라서 배경 색 바뀌게 */
+/* 시간에 따른 사이트 변경 */
 
 var day = new Date() 
 var time = day.getHours() 
-var bg = document.body;
 
-if (time >= 8 && time < 18) {
-	bg.className = "positive";	
+var bg = document.body;
+var postInfo = document.getElementById("post-info")
+
+if (time >= 7 && time < 18) {
+	bg.className = "positive";
+	meetusEmoji = document.createTextNode("☕");	
+	meetusMessage = "커피 한잔 고고고";
+	if (typeof(postInfo) != 'undefined' && postInfo != null) {
+		document.getElementById("prev-icon").src="../images/right-angle-bracket-dark.svg";
+		document.getElementById("next-icon").src="../images/left-angle-bracket-dark.svg";
+		document.getElementById("index-icon").src="../images/grid-layout-dark.svg";
+	}
 } else {
 	bg.className = "negative";
+	meetusEmoji = document.createTextNode("🍺");
+	meetusMessage = "역시 저녁에는 맥주입니다";
+	if (typeof(postInfo) != 'undefined' && postInfo != null) {
+		document.getElementById("prev-icon").src="../images/right-angle-bracket-light.svg";
+		document.getElementById("next-icon").src="../images/left-angle-bracket-light.svg";
+		document.getElementById("index-icon").src="../images/grid-layout-light.svg";
+	}
+}
+
+
+var meetusEmoji;
+document.getElementById("meetus").appendChild(meetusEmoji);
+function meetus() {
+	location.href="../contact";
+	alert(meetusMessage);
+	//document.getElementById("meetus-message").appendChild();
 }

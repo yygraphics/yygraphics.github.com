2c356ccf5ecd3fffe0cd6a9316e6b0256064b20d
yygraphics <picple.j@gmail.com>
2017-12-29 17:25:36 +0700

    some issues fixed

--- js/main.js
+++ js/main.js
@@ -11,17 +11,19 @@ var postInfo = document.getElementById("post-info")
 
 var meetusEmoji; 
 
-if (time >= 7 && time < 18) {
+if (time >= 7 && time < 12) {
 	bg.className = "positive";
 	meetusEmoji = document.createTextNode("☕");	
 	meetusMessage = "커피 한잔 고고고";
 } else {
-	document.getElementById("logo-img").src="../images/logo-light.svg"; //logo 변경
 	bg.className = "negative";
+	document.getElementById("logo-svg").style.fill="rgb(230,230,230)"; //logo 색 변경
+	
 	if (typeof(postInfo) != 'undefined' && postInfo != null) {
 		document.getElementById("prev-icon").src="../images/left-angle-bracket-light.svg"; //icon 변경
 		document.getElementById("next-icon").src="../images/right-angle-bracket-light.svg"; //icon 변경
 		document.getElementById("index-icon").src="../images/grid-layout-light.svg"; //icon 변경
+		document.getElementById("post-alt-title").style.borderTopColor="rgb(230,230,230)"; //알트헤더 보더 색 변경
 	}
 	meetusEmoji = document.createTextNode("🍺");
 	meetusMessage = "역시 저녁에는 맥주입니다";
@@ -35,36 +37,47 @@ function meetus() {
 	alert(meetusMessage);
 }
 
-
+// post page layout control
 if (typeof(postInfo) != 'undefined' && postInfo != null) {
-	// 화면 폭에 따라 gnb 지우기 
-	var windowWidth = window.innerWidth;
+	// 변경할 화면요소 가져오기
 	var gnb = document.getElementById("gnb")
-	if (windowWidth <= 840) {
-		gnb.style.display = "none";	
-	} else {
-		gnb.style.display = "block";	
-	}
-	window.onresize = function() {gnbRemove()} 
+	var postTitle = document.getElementById("post-title");
+	var altTitle = document.getElementById("post-alt-title");
+
+	var windowWidth = window.innerWidth; //화면넓이 정의
+	var visiblePoint = postTitle.offsetTop+40; //알트헤더 등장시점 정의
+
+	// 화면 로딩될 때 시점으로 gnb display 설정
+	gnbRemove();
 	function gnbRemove() {
-		windowWidth = window.innerWidth;
 		if (windowWidth <= 840) {
 			gnb.style.display = "none";	
 		} else {
 			gnb.style.display = "block";	
 		}
 	}
-	// 스크롤에 따른 글 제목 화면에 고정
-	var postTitle = document.getElementById("post-title");
-	var sticky = postTitle.offsetTop+4;
-	window.onscroll = function() {stickyItem()};
-	function stickyItem() {
-		if (window.pageYOffset >= sticky && windowWidth <= 1450) {
-		postTitle.classList.add("sticky");
+
+	// 스크롤에 따른 알트헤더 등장여부 설정
+	window.onscroll = function() {showAltTitle()};
+	function showAltTitle() {
+		if (window.pageYOffset >= visiblePoint && windowWidth <= 1450) {
+			altTitle.style.display = "block";
+			altTitle.classList.add("animation-downward");
 		} else {
-		postTitle.classList.remove("sticky");
+			altTitle.style.display = "none";
+			altTitle.classList.remove("animation-downward");
 		}
-	}	
+	}
+
+	// 화면 사이즈 변경될 때 레이아웃도 함께 갱신	
+	window.onresize = function() {windowResize()}; 
+	function windowResize() {
+		windowWidth = window.innerWidth;
+		visiblePoint = postTitle.offsetTop+40;
+		gnbRemove();
+		showAltTitle();
+	}
 }
+
 
 

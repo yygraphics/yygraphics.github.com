65b5f6b238a7f6dced8d180b2ef62669c6794910
yygraphics <picple.j@gmail.com>
2018-03-14 20:18:15 +0900

    헤더 아이콘 두꼐 수정

--- js/main.js
+++ js/main.js
@@ -232,40 +232,7 @@ function removeIOSRubberEffect(element) {
 	} );
 }
 
-// 모바일에서 마우스 오버 이펙트 막기
-function watchForHover() {
-    var hasHoverClass = false;
-    var container = document.body;
-    var lastTouchTime = 0;
 
-    function enableHover() {
-        // filter emulated events coming from touch events
-        if (new Date() - lastTouchTime < 500) return;
-        if (hasHoverClass) return;
-
-        container.className += ' hasHover';
-        hasHoverClass = true;
-    }
-
-    function disableHover() {
-        if (!hasHoverClass) return;
-
-        container.className = container.className.replace(' hasHover', '');
-        hasHoverClass = false;
-    }
-
-    function updateLastTouchTime() {
-        lastTouchTime = new Date();
-    }
-
-    document.addEventListener('touchstart', updateLastTouchTime, true);
-    document.addEventListener('touchstart', disableHover, true);
-    document.addEventListener('mousemove', enableHover, true);
-
-    enableHover();
-}
-
-watchForHover();
 
 /* 스크롤 할 때 텍스트에 이펙트 넣기
 window.onscroll = function() {bodyScroll()};
